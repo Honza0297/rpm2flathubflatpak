84731595e50466e3aecfe4263327d93a061b8649
Jan Beran <jaberan@redhat.com>
2021-09-16 12:53:57 +0200

    add supported buildsystems v0.1

--- r2ff.py
+++ r2ff.py
@@ -8,12 +8,11 @@ import subprocess
 # Get input args in the same format as "fedmod rpm2flatpak" command
 from_flathub = False
 flathub_id = None
+
 for arg in sys.argv:
     if "--flathub" == arg[0:9]:
         from_flathub = True
         flathub_id = arg[10:]
-
-with_flatpak_common  = "--flatpak-common" in sys.argv
 
 app_name = sys.argv[-1]
 
@@ -22,10 +21,9 @@ no_generate_files = "--no-gen" in sys.argv
 force_rewrite = "--force" in sys.argv
 
 # Generate app.yaml and container.yaml
-# TODO allow downloading the files from src.fedoraproject.org/flatpaks
+# TODO allow downloading the files from src.fedoraproject.org/flatpaks if they exist
 if not no_generate_files: 
     params = ["fedmod", "rpm2flatpak"]
-    params.append("--flatpak-common") if with_flatpak_common else None
     params.append("--flathub="+flathub_id) if from_flathub else None
     params.append("--force") if force_rewrite else None
     params.append(app_name)
@@ -44,25 +42,28 @@ manifest["command"] = container["flatpak"]["command"]
 manifest["finish-args"] = list(container["flatpak"]["finish-args"].split("\n"))
 manifest["modules"] = list()
 
-# convert fedora-like package listing to flathub-like
+# From this point: convert fedora-like package listing to flathub-like
 pkgs = []
-
 
 # get list of rpms used in fedora flatpak
 with open(app_name+".yaml", "r") as file:
     yaml_file = yaml.safe_load(file)
     pkgs = yaml_file["data"]["components"]["rpms"]
 
-# for every rpm:
-# get spec file, 
+
 url = "https://src.fedoraproject.org/rpms/{0}/raw/{1}/f/{0}.spec"
+sha_url = "https://src.fedoraproject.org/rpms/{0}/raw/{1}/f/sources"
+
+# For all rpms:
 for pkg_name in pkgs.keys():
+    # TODO check if app is in flathub's shared modules. If so, use the configuration from there.
+    
+    # Init the module
     module = json.loads("{}")
     module["name"] = pkg_name
     module["buildsystem"] = None
-    module["sources"] = list()
     module["config-opts"] = list() # TODO
-    
+    module["sources"] = list()    
 
     # TODO there has to be a better way to check whether we use private repo...
     try:
@@ -72,28 +73,39 @@ for pkg_name in pkgs.keys():
     else:    
         print("NOTE: Private repo in use for {}".format(pkg_name))
         pkg_url = None
-
     if not pkg_url:
         continue
     
+    # get spec file
     r = requests.get(pkg_url)
     specfile_raw = r.content.decode(encoding=r.encoding)
     specfile = Spec.from_string(specfile_raw)
     
 
-    ## pyrpm does not parse soe things, so soing it manually now:
+    ## pyrpm does not parse some things, so we have to do it manually for now:
     specfile_raw_lines = [line.rstrip() for line in specfile_raw.splitlines() if line != "\n"]
     for idx in range(len(specfile_raw_lines)):
         if specfile_raw_lines[idx] == "%build": # next line is build command
             buildsystem = specfile_raw_lines[idx+1].split(" ")[0]
             if buildsystem[0] == "%":
                 buildsystem = buildsystem[1:]
-            module["buildsystem"] = buildsystem
 
+            #in case of something else than cmake, qmake, meson, autotools, cmake-ninja, use "simple" and pop config-opts
+            supported_buildsystems = ["cmake", "qmake", "meson", "autotools", "cmake-ninja"]
+            if buildsystem not in supported_buildsystems:
+                print("Unsupported buildsystem:", buildsystem, ", application: ", pkg_name)
+                module["buildsystem"] = "simple"
+                module.pop("config-opts", None)
+                module["build-commands"] = list() # TODO
+            else:
+                module["buildsystem"] = buildsystem
+            
+
+    # Get sources
     if hasattr(specfile, "sources_dict"):
         for spec_src in specfile.sources_dict:
             source = json.loads("{}")
-            # Get source url and guess its type - archive, git, local file 
+            # Get source url and guess the type of source - archive, git, local file 
             src_url = replace_macros(specfile.sources_dict[spec_src], specfile)
             src_url_sliced = src_url.split("/")
             
@@ -105,9 +117,17 @@ for pkg_name in pkgs.keys():
                 source["type"] = "git"
 
             source["url"] = src_url
-            source["sha512"] = "TODO version (256, 512...) and checksum"
+
+            # get spec file
+            r = requests.get(sha_url.format(pkg_name, pkgs[pkg_name]["ref"]))
+            sha_file_sliced = r.content.decode(encoding=r.encoding).split(" ")
+
+            source[sha_file_sliced[0]] = sha_file_sliced[-1].rstrip()   
+            
+            # append current source to sources of the current module
             module["sources"].append(source)
 
+    # Get patches
     if hasattr(specfile, "patches_dict"):
         for spec_patch in specfile.patches_dict:
             patch = json.loads("{}")
